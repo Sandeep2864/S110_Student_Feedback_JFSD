e07c9b6049c9af26ef0aeb5e43c23cc6c619b574
Seelam Sandeep reddy <2200030956@kluniversity.in>
2024-10-08 19:28:11 +0530

    Add files via upload

---
+++ src/component/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+export default function Home() {
+
+  return (document.title = "Student Management System",
+    <div className='text-center'>
+        <h1 className='text-center'>Student Feedback and Evaluation System</h1>
+        <img src="https://www.questionpro.com/blog/wp-content/uploads/2022/05/student-feedback.jpg" alt='banner' className='logo img-fluid'/>
+    </div>
+  )
+}
